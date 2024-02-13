1fb00bbda7f36a3783f246d8c9a330e7cd9a769b
bankkasem <kasemsarn.purisarn@gmail.com>
2024-02-13 15:56:25 +0700

    refactor: :zap: refactor base code

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import { createPool } from "mysql2/promise";
+import ServerlessHttp from "serverless-http";
+import errorHandler from "./middlewares/errors";
+import foodRouter from "./routes/foods";
+
+export const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+export const handler = ServerlessHttp(app);
+
+export const pool = createPool({
+	connectionLimit: 1,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
+});
+
+app.get("/", (req, res) => {
+	res.status(200).json({
+		status: "OK",
+		request: { params: req.params, query: req.query, body: req.body },
+	});
+});
+
+app.use(foodRouter);
+app.use(errorHandler);
