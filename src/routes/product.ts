f5088466f19ec42110b3515e18a88c2f44834092
bankkasem <kasemsarn.purisarn@gmail.com>
2024-02-13 16:31:17 +0700

    feat: :sparkles: add new api for create product

---
+++ src/routes/product.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/products", async (_req, res, _next) => {
+	// ...
+	res.status(200).json({ result: "create product success!" });
+});
+
+export default router;
